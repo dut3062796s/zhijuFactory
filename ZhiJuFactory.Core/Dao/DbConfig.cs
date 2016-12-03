b5bf60a63b5acb2da8418e50e6524c3b147b4a08
杜鹏 <511202838@qq.com>
2016-12-03 14:00:58 +0800

    添加Dao文件

---
+++ ZhiJuFactory.Core/Dao/DbConfig.cs
@@ -0,0 +1,38 @@
+﻿using MySqlSugar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ZhiJuFactory.Core.Dao
+{
+    public class DbConfig
+    {
+        public static string ConnectionString =  ConfigSugar.GetConfigString("DefaultConnection");
+        public static Dictionary<string, Func<KeyValueObj>> DefaultFilter = new Dictionary<string, Func<KeyValueObj>>() {
+            //单表查询
+            { "FalseDelte",()=>{return new KeyValueObj(){ Key=" (isdeleted=0  or isdeleted is null )"};} },
+            //多表查询
+            { "FalseDelteJoin",()=>{return new KeyValueObj(){ Key=" (m.isdeleted=0  or m.isdeleted is null )"};} } };
+        public static SqlSugarClient GetDbInstance()
+        {
+            try
+            {
+                var reval = new SqlSugarClient(ConnectionString);
+                reval.SetFilterItems(DefaultFilter);//给查询添加默认过滤器 （所有查询加上 isdeleted=0 or null）
+                reval.AddDisableUpdateColumns("CreateTime", "Creator");//添加禁止更新列
+                reval.CurrentFilterKey = "FalseDelte";
+                reval.IsEnableLogEvent = true;
+                reval.LogEventStarting = (sql, pars) =>
+                {//在这儿打段点可以查看生成的SQL语句
+
+                };
+                return reval;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("连接数据库出错，请检查您的连接字符串，和网络。 ex:".AppendString(ex.Message));
+            }
+        }
+    }
+}
---
+++ ZhiJuFactory.Core/Dao/DbService.cs
@@ -0,0 +1,37 @@
+﻿using MySqlSugar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ZhiJuFactory.Core.Dao
+{
+    /// <summary>
+    ///数据访问层
+    /// </summary>
+    public class DbService : IDisposable
+    {
+        private SqlSugarClient _db;
+        public DbService()
+        {
+            _db = DbConfig.GetDbInstance();
+        }
+
+        /// <summary>
+        /// 将错误信息写入日志
+        /// </summary>
+        /// <param name="ex"></param>
+        private static void WriteExMessage(Exception ex)
+        {
+            //PubMethod.WirteExp(ex);
+        }
+
+        public void Dispose()
+        {
+            if (_db != null)
+            {
+                _db.Dispose();
+            }
+        }
+    }
+}
